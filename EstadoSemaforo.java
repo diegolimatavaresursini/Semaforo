1b8c8714c8936d00a6183ce3331b87683afe911e
Diego Lima Tavares Ursini <73371155+diegolimatavaresursini@users.noreply.github.com>
2024-08-22 06:25:55 -0300

    Create EstadoSemaforo.java

---
+++ EstadoSemaforo.java
@@ -0,0 +1,11 @@
+public class EstadoSemaforo {
+    public static void main(String[] args) {
+        String semaforo = "vermelho";
+
+        if (semaforo.equals("vermelho")) {
+            System.out.println("Pare");
+        } else {
+            System.out.println("Prossiga");
+        }
+    }
+}
